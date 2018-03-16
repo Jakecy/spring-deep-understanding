ad08097767dca8ac432df4fc145e3181b63317fd
chj <Chj411628>
2018-03-16 18:11:49 +0800

    测试environment对象

--- study-spring-aop-Aspect/src/main/java/com/SpringDemoApplication.java
+++ study-spring-aop-Aspect/src/main/java/com/SpringDemoApplication.java
@@ -2,6 +2,7 @@ package com;
 
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 import com.config.AopConfig;
 import com.service.DemoAnnotationService;
@@ -11,6 +12,9 @@ public class SpringDemoApplication {
 
 	public static void main(String[] args) {
 		AnnotationConfigApplicationContext context=new AnnotationConfigApplicationContext(AopConfig.class);
+		//
+		ConfigurableEnvironment environment = context.getEnvironment();
+		
 		DemoAnnotationService annotationService = context.getBean(DemoAnnotationService.class);
 		annotationService.add();
 		System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
